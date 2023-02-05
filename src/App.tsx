153425c284262382d5f5219289fd6ed7315539e4
headring <myhakiki@gmail.com>
2023-02-06 02:56:56 +0900

    refactor: session을 이용하여 데이터 저장하여 persist 완료

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,63 @@ import { Pagination } from "./components/Pagination";
 import Search from "./components/Search";
 import Table from "./components/Table";
 import useDataFetch from "./useDataFetch";
+import "./main.sass";
 
 export interface Products {
   [index: string]: string | number | string[];
+  title: string;
+  brand: string;
+  description: string;
 }
-const tableHeaders = [
-  "상품번호",
-  "상품명",
-  "브랜드",
-  "싱품내용",
-  "가격",
-  "평점",
-  "재고",
-];
 
 function App() {
-  // const [skip, setSkip] = useState(0);
-  const [products, setProducts] = useState<Products[]>([]);
+  const [totalProducts, setTotalProducts] = useState<Products[]>([]);
+  const [renderProducts, setRenderProducts] = useState<Products[]>([]);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState<number>(0);
   const offset = (page - 1) * limit;
+  const [isSearch, setIsSearch] = useState(false);
 
-  const { refetch, isSuccess, data } = useDataFetch();
+  const [option, setOption] = useState("전체");
+  const [searchTxt, setSearchTxt] = useState("");
+
+  /** React-Query 데이터 받아오고 상태 저장하기 */
+  useEffect(() => {
+    setTotal(renderProducts.length);
+  }, [renderProducts]);
+  const { data } = useDataFetch();
   useEffect(() => {
     if (data) {
-      setProducts(data.products);
+      setTotalProducts(data.products);
       setTotal(data.products.length);
+      setOption(sessionStorage.getItem("option") as string);
+      setSearchTxt(sessionStorage.getItem("searchTxt") as string);
+      const parse = JSON.parse(sessionStorage.getItem("result") as string);
+      console.log(parse);
+      if (parse.length !== 0) {
+        setRenderProducts(parse);
+      } else {
+        setRenderProducts(data.products);
+      }
     }
-  }, [isSuccess, data]);
-  // useEffect(() => {
-  //   refetch();
-  // }, [limit, skip]);
+  }, [data]);
+
   return (
-    <div>
-      <Search />
+    <div className="main">
+      <Search
+        option={option}
+        setOption={setOption}
+        searchTxt={searchTxt}
+        setSearchTxt={setSearchTxt}
+        renderProducts={renderProducts}
+        setRenderProducts={setRenderProducts}
+        totalProducts={totalProducts}
+        isSearch={isSearch}
+        setIsSearch={setIsSearch}
+      />
       <div>검색한 테이터: {total}건</div>
-      <Table offset={offset} products={products} limit={limit} />
+      <Table offset={offset} renderProducts={renderProducts} limit={limit} />
       <Pagination
         total={total}
         limit={limit}
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,94 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import { Products } from "../App";
 
-const Search = () => {
+interface Props {
+  option: string;
+  setOption: React.Dispatch<React.SetStateAction<string>>;
+  searchTxt: string;
+  setSearchTxt: React.Dispatch<React.SetStateAction<string>>;
+  renderProducts: Products[];
+  setRenderProducts: React.Dispatch<React.SetStateAction<Products[]>>;
+  totalProducts: Products[];
+  isSearch: boolean;
+  setIsSearch: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Search = ({
+  option,
+  setOption,
+  searchTxt,
+  setSearchTxt,
+  renderProducts,
+  setRenderProducts,
+  totalProducts,
+  isSearch,
+  setIsSearch,
+}: Props) => {
+  const mounted = useRef(false);
+
+  const onClick = () => {
+    sessionStorage.setItem("option", option);
+    sessionStorage.setItem("searchTxt", searchTxt);
+    if (option === "전체") {
+      setRenderProducts(
+        totalProducts.filter((product) => {
+          return (
+            product.title.includes(searchTxt) ||
+            product.brand.includes(searchTxt) ||
+            product.description.includes(searchTxt)
+          );
+        })
+      );
+    }
+    if (option === "상품명") {
+      setRenderProducts(
+        totalProducts.filter((product) => product.title.includes(searchTxt))
+      );
+    }
+    if (option === "브랜드") {
+      setRenderProducts(
+        totalProducts.filter((product) => product.brand.includes(searchTxt))
+      );
+    }
+    if (option === "상품내용") {
+      setRenderProducts(
+        totalProducts.filter((product) =>
+          product.description.includes(searchTxt)
+        )
+      );
+    }
+    setIsSearch((prev) => !prev);
+    mounted.current = true;
+  };
+  useEffect(() => {
+    console.log(mounted);
+    if (mounted.current) {
+      console.log("why");
+      const prevResult = JSON.stringify(renderProducts);
+      sessionStorage.setItem("result", prevResult);
+    }
+  }, [isSearch]);
+
   return (
     <div>
       <div>검색</div>
+      <select
+        value={option}
+        onChange={({ target: { value } }) => setOption(value)}
+      >
+        <option value="전체">젠체</option>
+        <option value="상품명">상품명</option>
+        <option value="브랜드">브랜드</option>
+        <option value="상품내용">상품내용</option>
+      </select>
       <div>
-        <input />
+        <input
+          value={searchTxt}
+          onChange={(e) => setSearchTxt(e.target.value)}
+        />
       </div>
-      <button>조회</button>
+      <button onClick={onClick}>조회</button>
     </div>
   );
 };
-
 export default Search;
